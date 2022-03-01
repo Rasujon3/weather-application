f463db16447f1d5709ec5c39cf796b91fb55af90
Rasujon3 <rasujon3@gmail.com>
2022-03-01 23:21:30 +0600

    37_5-2 Display temperature, city, dynamic weather icon

--- js/app.js
+++ js/app.js
@@ -2,12 +2,22 @@ const API_KEY = `075b6fd1c138103dce75d3793e35893e`;
 
 const searchTemperature = () => {
   const city = document.getElementById("city-name").value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayTemperature(data));
 };
 
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
+};
+
 const displayTemperature = (temperature) => {
-  console.log(temperature);
+  setInnerText("city", temperature.name);
+  setInnerText("temperature", temperature.main.temp);
+  setInnerText("condition", temperature.weather[0].main);
+  // set weather icon
+  const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imageIcon = document.getElementById("weather-icon");
+  imageIcon.setAttribute("src", url);
 };

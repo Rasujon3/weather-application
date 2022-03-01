d7e55330709900de81436cba1525d35976d9589c
Rasujon3 <rasujon3@gmail.com>
2022-03-01 23:03:20 +0600

    37_5-1 Get API key and load temperature data by city name

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const API_KEY = `075b6fd1c138103dce75d3793e35893e`;
+
+const searchTemperature = () => {
+  const city = document.getElementById("city-name").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTemperature(data));
+};
+
+const displayTemperature = (temperature) => {
+  console.log(temperature);
+};
